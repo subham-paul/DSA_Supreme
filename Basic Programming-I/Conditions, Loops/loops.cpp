17ed5c2723ad339ea1fcda8baca100e661374aea
Subham <subhampaul456@gmail.com>
2023-01-29 13:08:29 +0530

    Conditions

---
+++ Basic Programming-I/Conditions, Loops/loops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /* for (int i = 5; i >= 1; i--)
+    {
+        cout<<i<<endl;
+    } */
+
+    /* for (int i = 1; i <= 10; i++)
+    {
+        cout << "2 * " << i << " is => " << i * 2 << endl;
+    } */
+
+    /* for (int i = 10; i >= 1; i--)
+    {
+        cout << "3 * " << i << " is => " << i * 3 << endl;
+    } */
+
+    
+    
+    return 0;
+}
