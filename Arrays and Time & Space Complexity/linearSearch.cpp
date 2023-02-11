cfba3e51c1edede165891efc15d0af040c52de38
Subham <subhampaul456@gmail.com>
2023-02-11 16:33:26 +0530

    Arrays

---
+++ Arrays and Time & Space Complexity/linearSearch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+bool linearSearch(int arr[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == key)
+        {
+            return true;
+        }
+    }
+    return false;
+};
+int main()
+{
+    int arr[] = {11, 22, 33, 44, 55, 66};
+    int size = 6;
+    int key = 5;
+    if (linearSearch(arr, size, key))
+    {
+        cout << "Found value";
+    }
+    else
+    {
+        cout << "Not Found Value";
+    }
+    return 0;
+}
