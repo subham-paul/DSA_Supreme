319b06b18acb8e8a66de4362958ef330473d0eee
Subham <subhampaul456@gmail.com>
2023-02-13 18:50:59 +0530

    Find Unique Element

--- Arrays and Time & Space Complexity/findUniqueElement.cpp
+++ Arrays and Time & Space Complexity/findUniqueElement.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 int findUnique(vector<int> arr) {
 	int ans = 0;
-
 	for(int i=0; i<arr.size(); i++) {
 		ans = ans ^ arr[i];
 	}
@@ -13,16 +12,13 @@ int main(){
     int n;
 	cout << "Enter the size of array -> ";
 	cin >> n;
-
 	vector<int> arr(n);
 	cout << "Enter the elements " << endl;
 	//taking input
 	for(int i=0; i<arr.size(); i++) {
 		cin >> arr[i];
 	}
-
 	int uniqueElement = findUnique(arr);
-
 	cout << "Unique Element is  " << uniqueElement << endl;
     return 0;
 }
