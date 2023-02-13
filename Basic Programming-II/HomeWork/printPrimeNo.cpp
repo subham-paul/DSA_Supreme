cf9f8a692d00fecf4a8ae2abef46edefdaa59b2e
subham-paul <subhampaul456@gmail.com>
2023-02-13 08:49:06 +0530

    PRINT PRIME NUMBER

--- Basic Programming-II/HomeWork/printPrimeNo.cpp
+++ Basic Programming-II/HomeWork/printPrimeNo.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
+
 bool check_prime(int n) {
     for (int i = 2; i < n; i++) {
-        if(n%i == 0){
+        if (n % i == 0) {
             return false;
         }
     }
     return true;
 }
 
-int main()
-{
+int main() {
     int n;
     cout << "Enter a number -> ";
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        bool isPrime
+    for (int i = 2; i < n; i++) {
+        bool isPrime = check_prime(i);
+        if (isPrime) {
+            cout << i << " ";
+        }
     }
-    
+    cout << endl;
     return 0;
 }
