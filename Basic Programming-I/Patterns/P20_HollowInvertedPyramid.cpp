afb9575d669408cea5816262a3a47865210007c8
subham-paul <subhampaul456@gmail.com>
2023-02-14 18:04:38 +0530

    HomeWork week 2

---
+++ Basic Programming-I/Patterns/P20_HollowInvertedPyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number -> ";
+    cin >> n;
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = row + 1; col <= n; col++)
+        {
+            if (row == 0 || col == row + 1 || col == n)
+            {
+                cout << col;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
