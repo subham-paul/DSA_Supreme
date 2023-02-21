a6aae8d487c17e77957b9a944558c1f2642d490e
Subham <subhampaul456@gmail.com>
2023-02-21 11:51:34 +0530

    Extreame Print

--- Arrays and Time & Space Complexity/extremePrint.cpp
+++ Arrays and Time & Space Complexity/extremePrint.cpp
@@ -1,3 +1,6 @@
+// For Extreame Print in an array Like Input = {1,2,3,4,5,6,7,8}   Output = {1,8,2,7,3,6,4,5}
+// Here I using 2 pointer approach and print start and end digit of an array
+
 #include <iostream>
 using namespace std;
 int main() {
