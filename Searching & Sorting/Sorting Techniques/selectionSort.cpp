a0013ac531235e3be02b13b083ca47d730b146c4
subham-paul <subhampaul456@gmail.com>
2023-02-25 11:15:08 +0530

    Selection sort

---
+++ Searching & Sorting/Sorting Techniques/selectionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> arr{5, 4, 3, 2, 1};
+    int n = arr.size();
+    // selection sort
+    // outer loop -> (n-1) rounds
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        // inner Loop -> index of minimum element in range i->n
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                // new minimum, then store
+                minIndex = j;
+            }
+        }
+        // swap
+        swap(arr[i], arr[minIndex]);
+    }
+
+    // printing output
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    for(auto val : arr){
+        cout<<val<<", ";
+    }
+    cout << endl;
+
+    return 0;
+}
