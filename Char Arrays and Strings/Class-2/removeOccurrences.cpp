39df6d432523ef571c9f6d10d7d73211cbd3fe48
subham-paul <subhampaul456@gmail.com>
2023-02-28 12:16:57 +0530

    class 2 leetcode problem done

--- Char Arrays and Strings/Class-2/removeOccurrences.cpp
+++ Char Arrays and Strings/Class-2/removeOccurrences.cpp
@@ -1,11 +1,19 @@
-class Solution {
-public:
-    string removeOccurrences(string s, string part) {
-        int pos = s.find(part);
-        while(pos != string::npos) {
-            s.erase(pos, part.length());
-            pos = s.find(part);
-        }
-        return s;
+#include <iostream>
+using namespace std;
+string removeOccurrences(string s, string part)
+{
+    int pos = s.find(part);
+    while (pos != string::npos)
+    {
+        s.erase(pos, part.length());
+        pos = s.find(part);
     }
-};
+    return s;
+}
+int main()
+{
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    cout << removeOccurrences(s, part) << endl;
+    return 0;
+}
--- Char Arrays and Strings/Class-2/validPalindrome.cpp
+++ Char Arrays and Strings/Class-2/validPalindrome.cpp
@@ -1,25 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    bool checkPalindrome(string s, int i, int j ) {
-        while(i <= j) {
-            if(s[i] != s[j] )
+    bool checkPalindrome(string s, int i, int j){
+        while (i <= j){
+            if (s[i] != s[j])
                 return false;
             i++;
             j--;
         }
         return true;
     }
-    bool validPalindrome(string s) {
+    bool validPalindrome(string s){
         int i = 0;
         int j = s.length() - 1;
 
-        while(i<=j) {
-            if(s[i] != s[j]) {
-                //ek baar i ko remove, ek baar j ko remove
-                return checkPalindrome(s,i+1, j) || checkPalindrome(s, i, j-1);
-            }
-            else {
-                //s[i] == s[j]
+        while (i <= j){
+            if (s[i] != s[j]){
+                // ek baar i ko remove, ek baar j ko remove
+                return checkPalindrome(s, i + 1, j) || checkPalindrome(s, i, j - 1);
+            } else {
+                // s[i] == s[j]
                 i++;
                 j--;
             }
@@ -27,9 +27,3 @@ public:
         return true;
     }
 };
-
-
-
-
-
-
