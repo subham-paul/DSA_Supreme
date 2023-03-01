3a7f5f004b30cd52dc6bf8b69a96275c9d51ee47
Subham <subhampaul456@gmail.com>
2023-03-01 09:41:46 +0530

    waveMatrix

--- Arrays and Time & Space Complexity/Week-3 Assignments/waveMatrix.cpp
+++ Arrays and Time & Space Complexity/Week-3 Assignments/waveMatrix.cpp
@@ -27,12 +27,16 @@ void waveMatrix(vector<vector<int>> v)
 }
 int main()
 {
-    vector<vector<int>> v{
+    /* vector<vector<int>> v{
         {1, 2, 3, 4},
         {5, 6, 7, 8},
         {9, 10, 11, 12},
         {13, 14, 15, 16},
-        {17, 18, 19, 20}};
+        {17, 18, 19, 20}}; */
+        vector<vector<int>> v{
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}};
     waveMatrix(v);
 
     return 0;
