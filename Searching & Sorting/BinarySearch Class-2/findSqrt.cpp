b15c6a41c73493ff14500344149902119afe31dc
subham-paul <subhampaul456@gmail.com>
2023-03-07 11:16:53 +0530

    findSqrt

--- Searching & Sorting/BinarySearch Class-2/findSqrt.cpp
+++ Searching & Sorting/BinarySearch Class-2/findSqrt.cpp
@@ -37,19 +37,19 @@ int main() {
   int ans = findSqrt(n);
   cout << "Ans is " << ans << endl;
 
-  int precision;
-  cout << "Enter the number of floating digits in precison " << endl;
-  cin >> precision;
+  // int precision;
+  // cout << "Enter the number of floating digits in precison " << endl;
+  // cin >> precision;
 
-  double step = 0.1;
-  double finalAns = ans;
-  for (int i = 0; i < precision; i++) {
+  // double step = 0.1;
+  // double finalAns = ans;
+  // for (int i = 0; i < precision; i++) {
 
-    for (double j = finalAns; j * j <= n; j = j + step) {
-      finalAns = j;
-    }
-    step = step / 10;
-  }
-  cout << "Final ans is. " << finalAns << endl;
+  //   for (double j = finalAns; j * j <= n; j = j + step) {
+  //     finalAns = j;
+  //   }
+  //   step = step / 10;
+  // }
+  // cout << "Final ans is. " << finalAns << endl;
   return 0;
 }
