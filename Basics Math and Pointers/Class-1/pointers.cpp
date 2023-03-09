0b064175e74e03657f3aca63598b5c27be61545b
subham-paul <subhampaul456@gmail.com>
2023-03-09 20:39:54 +0530

    Pointer class 1

--- Basics Math and Pointers/Class-1/pointers.cpp
+++ Basics Math and Pointers/Class-1/pointers.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  //   int a = 5;
-  //   int b = 5;
+int main() {
+  
+  // int a = 5;
+  // int b = 5;
 
-  //   cout << a << endl;
+  // cout << a << endl;
 
-  //   cout << &a << endl;
-  //   cout << &b << endl;
+  // cout << &a << endl;
+  // cout << &b << endl;
 
   // int a = 5;
-  // //pointer create
-  // int* ptr = &a;
+  // // pointer create
+  // int *ptr = &a;
 
-  // //access the value ptr is pointing to
+  // // access the value ptr is pointing to
   // cout << "Address of a is: " << &a << endl;
-  // cout <<"value stored at ptr is: " << ptr << endl;
-  // cout <<"value ptr is pointing to is: " << *ptr << endl;
-  // cout << &ptr << endl;
+  // cout << "value stored at ptr or a address is: " << ptr << endl;
+  // cout << "value ptr is pointing to is: " << *ptr << endl;
+  // cout << "Address of ptr is: " << &ptr << endl;
 
-  // int a = 5;
-  // int* p = &a ;
+  // int n = 5;
+  // int *_n = &n;
 
-  // cout <<sizeof(p) << endl;
+  // bool isPrime = true;
+  // bool *_isPrime = &isPrime;
+
+  // char ch = 'A';
+  // char *_ch = &ch;
+
+  // float number = 26.228;
+  // float *_number = &number;
+
+  // double dNumber = 3.06;
+  // double *_dNumber = &dNumber;
+
+  // string name = "subham";
+  // string *_name = &name;
+
+  // // large integer
+  // long lNumber = 123456;
+  // long* _lNumber = &lNumber;
+
+  // // long long int
+  // long long llNumber = 12345678;
+  // long long *_llNumber = &llNumber;
+
+  // cout << "Size of Int Pointer -> " << sizeof(_n) << endl;
+  // cout << "Size of Bool Pointer -> " << sizeof(_isPrime) << endl;
+  // cout << "Size of char Pointer -> " << sizeof(_ch) << endl;
+  // cout << "Size of Float Pointer -> " << sizeof(_number) << endl;
+  // cout << "Size of Double Pointer -> " << sizeof(_dNumber) << endl;
+  // cout << "Size of string Pointer -> " << sizeof(_name) << endl;
+  // cout << "Size of long Pointer -> " << sizeof(_lNumber) << endl;
+  // cout << "Size of long long Pointer -> " << sizeof(_llNumber) << endl;
 
   // char ch = 'b';
   // char* c = &ch ;
@@ -56,25 +86,25 @@ int main()
   // cout << *ptr << endl;
   // cout << *dusraPtr << endl;
 
-  int a = 10;  // Value of a
-  int *p = &a; // p is a pointer to integer value
-  int *q = p;  // q is a pointer to integer value
-  int *r = q;  // r is a pointer to integer value
+  // int a = 10;  // Value of a
+  // int *p = &a; // p is a pointer to integer value
+  // int *q = p;  // q is a pointer to integer value
+  // int *r = q;  // r is a pointer to integer value
 
-  cout << a << endl;                   // 10
-  cout << &a << endl;                  // address of a
-  cout << p << endl;                   // address of a
-  cout << &p << endl;                  // address of p
-  cout << *p << endl;                  // 10
-  cout << q << endl;                   // address of a
-  cout << &q << endl;                  // address of q
-  cout << *q << endl;                  // 10
-  cout << r << endl;                   // address of a
-  cout << &r << endl;                  // address of r
-  cout << *r << endl;                  // 10
-  cout << (*p + *q + *r) << endl;      // 30
-  cout << (*p) * 2 + (*r) * 3 << endl; // 50
-  cout << (*p / 2) - (*q) / 2 << endl; // 0
+  // cout << a << endl;                   // 10
+  // cout << &a << endl;                  // address of a
+  // cout << p << endl;                   // address of a
+  // cout << &p << endl;                  // address of p
+  // cout << *p << endl;                  // 10
+  // cout << q << endl;                   // address of a
+  // cout << &q << endl;                  // address of q
+  // cout << *q << endl;                  // 10
+  // cout << r << endl;                   // address of a
+  // cout << &r << endl;                  // address of r
+  // cout << *r << endl;                  // 10
+  // cout << (*p + *q + *r) << endl;      // 30
+  // cout << (*p) * 2 + (*r) * 3 << endl; // 50
+  // cout << (*p / 2) - (*q) / 2 << endl; // 0
 
   return 0;
 }
