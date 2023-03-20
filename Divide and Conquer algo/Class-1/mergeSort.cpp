f42c1513b551fa1c83cb32bb9b58393d57e55ad9
subham-paul <subhampaul456@gmail.com>
2023-03-20 10:51:11 +0530

    merge sort inComplete

---
+++ Divide and Conquer algo/Class-1/mergeSort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int mergeSort(int arr[], int n){
+    
+}
+
+int main(){
+    int arr[] = {7,3,2,16,24,4,11,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<mergeSortUsingRecursion(arr,n)<<endl;
+    return 0;
+}
