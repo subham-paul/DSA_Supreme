190611ca291db68791450490ecde14dc33808362
Subham <subhampaul456@gmail.com>
2023-04-04 13:58:44 +0530

    sieve

--- Basics Math and Pointers/Optimising & Segmented Sieve/primeNo.cpp
+++ Basics Math and Pointers/Optimising & Segmented Sieve/primeNo.cpp
@@ -3,21 +3,18 @@
 #include <cmath>
 using namespace std;
 
-vector<bool> sieve(int n)
-{
+vector<bool> sieve(int n) {
     // create a sieve array of n size telling isPrime
     vector<bool> _sieve(n + 1, true);
     _sieve[0] = _sieve[1] = false;
 
     for (int i = 2; i * i <= n; i++) // Optimisation 2, OuterLoop i<=root n
     {
-        if (_sieve[i] == true)
-        {
+        if (_sieve[i] == true) {
             // int j = i * 2;
             int j = i * i; // Optimisation 1
             // First unMarked number would be i*i, as others have been marked by 2 to (i-1)
-            while (j <= n)
-            {
+            while (j <= n) {
                 _sieve[j] = false;
                 j += i;
             }
@@ -26,13 +23,11 @@ vector<bool> sieve(int n)
     return _sieve;
 }
 
-vector<bool> segSieve(int L, int R)
-{
+vector<bool> segSieve(int L, int R) {
     // get me prime array, I will use it to mark seg sieve.
     vector<bool> _sieve = sieve(sqrt(R));
     vector<int> basePrimes;
-    for (int i = 0; i < _sieve.size(); i++)
-    {
+    for (int i = 0; i < _sieve.size(); i++) {
         if (_sieve[i])
         {
             basePrimes.push_back(i);
@@ -62,8 +57,7 @@ vector<bool> segSieve(int L, int R)
     return segSieve;
 }
 
-int main()
-{
+int main() {
     // vector<bool> _sieve = sieve(25);
     // for (int i = 0; i <= 25; i++)
     // {
@@ -75,8 +69,7 @@ int main()
     int L = 110;
     int R = 130;
     vector<bool> ss = segSieve(L, R);
-    for (int i = 0; i < ss.size(); i++)
-    {
+    for (int i = 0; i < ss.size(); i++) {
         if (ss[i])
         {
             cout << i + L << " ";
