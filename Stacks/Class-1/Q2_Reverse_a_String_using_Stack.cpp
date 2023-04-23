b23486afc38684c984353fecdd3c25c841a9179c
subham-paul <subhampaul456@gmail.com>
2023-04-23 17:42:43 +0530

    Stack 1

---
+++ Stacks/Class-1/Q2_Reverse_a_String_using_Stack.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void reverse(string str){
+    stack<char>s;
+    for(int i = 0;i<str.length();i++){
+        s.push(str[i]);
+    }
+    while(!s.empty()){
+        cout<<s.top();
+        s.pop();
+    }
+
+}
+int main(){
+    string str = "SUBHAM";
+    reverse(str);
+    cout<<endl;
+    return 0;
+}
