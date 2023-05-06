572b56b92d8c6a8fe1e60b66f5e32633fa77be25
subham-paul <subhampaul456@gmail.com>
2023-05-06 09:06:20 +0530

    friend function

--- OOPs/friendKeyword.cpp
+++ OOPs/friendKeyword.cpp
@@ -5,8 +5,8 @@ class A {
         int x;
     public:
         A(int _val) : x(_val){};
-        
-        int getX() const { return; }
+
+        int getX() const { return x; }
         void setX(int _val) { x = _val; }
 
         friend class B;
@@ -15,12 +15,17 @@ class A {
 class B {
     public:
         void print(const A &a){
-            cout<<a.x<<endl;
+            cout << a.x << endl;
         }
 };
+
+void print(const A &a) {
+    cout << a.x << endl;
+}
 int main() {
     A a(5);
     B b;
     b.print(a);
+    print(a);
     return 0;
 }
