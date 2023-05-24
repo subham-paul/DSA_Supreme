52106e92ff1ce9764093adf097e35f7f7774afb1
subham-paul <subhampaul456@gmail.com>
2023-05-24 20:42:15 +0530

    class-1

--- Heaps/Class-1/Q1_Insertion_in_Heap.cpp
+++ Heaps/Class-1/Q1_Insertion_in_Heap.cpp
@@ -62,13 +62,13 @@ public:
 
 int main() {
     Heap h;
-    //   h.arr[0] = -1;
-    //   h.arr[1] = 100;
-    //   h.arr[2] = 50;
-    //   h.arr[3] = 60;
-    //   h.arr[4] = 40;
-    //   h.arr[5] = 45;
-    //   h.size = 5;
+    // h.arr[0] = -1;
+    // h.arr[1] = 100;
+    // h.arr[2] = 50;
+    // h.arr[3] = 60;
+    // h.arr[4] = 40;
+    // h.arr[5] = 45;
+    // h.size = 5;
     h.insert(50);
     h.insert(30);
     h.insert(70);
