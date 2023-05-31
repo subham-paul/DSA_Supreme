bbfe69d816455c8c6359a59f42becb3055d71ec9
subham-paul <subhampaul456@gmail.com>
2023-05-31 21:17:20 +0530

    wip

---
+++ Linked List/Debug_Exercise/Qns2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int data;
+    ListNode *next;
+};
+
+ListNode *head;
+
+void insert(int x) {
+    ListNode *temp = new ListNode;
+    temp->data = x;
+    temp->next = NULL;
+    if(head == NULL) {
+        head = temp;
+    } else {
+        ListNode* current = head;
+        while(current->next != NULL) {
+            current = current->next;
+        }
+        current->next = temp;
+    }
+}
+
+ListNode* middleNode(ListNode* head) {
+        ListNode* last = head;
+        ListNode* first = head;
+
+        while (first != NULL && first->next != NULL) { //Fix
+            last = last->next;
+            first = first->next->next;
+        }
+    
+        return last;
+    }
+
+void print() {
+    ListNode *temp = head;
+    cout << "List: ";
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    head = NULL;
+    insert(1);
+    insert(2);
+    insert(3);
+    print();
+    return 0;
+}
