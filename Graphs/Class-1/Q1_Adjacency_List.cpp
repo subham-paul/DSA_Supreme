e0c44b0be008aede207212294bcd5856d6ac578a
Subham <subhampaul456@gmail.com>
2023-06-25 12:09:05 +0530

    code update

--- Graphs/Class-1/Q1_Adjacency_List.cpp
+++ Graphs/Class-1/Q1_Adjacency_List.cpp
@@ -1,33 +1,33 @@
-#include<iostream>
-#include<unordered_map>
-#include<list>
+#include <iostream>
+#include <unordered_map>
+#include <list>
 using namespace std;
 
-class Graph{
+class Graph {
 public:
     // create map form src with neighbours
-    unordered_map<int,list<int>>adjList;
+    unordered_map<int, list<int>> adjList;
 
-    void addEdge(int u,int v,bool direction){
+    void addEdge(int u, int v, bool direction) {
         // direction = 0 -> undirected
-        // direction = 1 -> directed 
+        // direction = 1 -> directed
         // Create an edge from u to v
         adjList[u].push_back(v);
-        // if undirected 
-        if(direction == 0)
-            adjList[v].push_back(u); // edge created from v to u    
+        // if undirected
+        if (direction == 0)
+            adjList[v].push_back(u); // edge created from v to u
     }
-    void printAdjList(){
-        for(auto node : adjList){
-            cout<<node.first<<"-> ";
-            for(auto neighbours : node.second){
-                cout<<neighbours<<", ";
+    void printAdjList() {
+        for (auto node : adjList) {
+            cout << node.first << "-> ";
+            for (auto neighbours : node.second) {
+                cout << neighbours << ", ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
 };
-int main(){
+int main() {
     Graph g;
     // undirected graph
     // g.addEdge(0,1,0);
@@ -35,9 +35,9 @@ int main(){
     // g.addEdge(0,2,0);
 
     // directed graph
-    g.addEdge(1,2,1);
-    g.addEdge(0,1,1);
-    g.addEdge(0,2,1);
+    g.addEdge(1, 2, 1);
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 2, 1);
 
     g.printAdjList();
     return 0;
