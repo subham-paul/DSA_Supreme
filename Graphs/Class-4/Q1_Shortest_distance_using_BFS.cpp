b4b4f69c452b987b8cd7c3f2a4ecaa8f5ec33d33
subham-paul <subhampaul456@gmail.com>
2023-06-28 15:05:40 +0530

    code update

--- Graphs/Class-4/Q1_Shortest_distance_using_BFS.cpp
+++ Graphs/Class-4/Q1_Shortest_distance_using_BFS.cpp
@@ -1,79 +1,82 @@
-#include<iostream>
-#include<unordered_map>
-#include<list>
-#include<queue>
-#include<algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-class Graph{
-    public:
-    unordered_map<int,list<pair<int,int>>>adjList;
-    void addEdge(int u,int v,int wt,bool direction){
+class Graph {
+public:
+    unordered_map<int, list<pair<int, int>>> adjList;
+    void addEdge(int u, int v, int wt, bool direction) {
         // direction = 1 -> undirected
         // direction = 0 -> directed
-        adjList[u].push_back({v,wt});
-        if(direction == 1){
-            adjList[v].push_back({u,wt});
+        adjList[u].push_back({v, wt});
+        if (direction == 1) {
+            adjList[v].push_back({u, wt});
         }
     }
-    void printAdjList(){
-        for(auto i : adjList){
-            cout<<i.first<<"-> ";
-            for(auto j : i.second){
-                cout<<"("<<j.first<<","<<j.second<<"), ";
+    void printAdjList() {
+        for (auto i : adjList) {
+            cout << i.first << "-> ";
+            for (auto j : i.second) {
+                cout << "(" << j.first << "," << j.second << "), ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-    void shortestPath(int src,int dest){
-        queue<int>q;
-        unordered_map<int,bool>visited;
-        unordered_map<int,int>parent;
+    void shortestPath(int src, int dest) {
+        queue<int> q;
+        unordered_map<int, bool> visited;
+        unordered_map<int, int> parent;
         // initial steps for src
         q.push(src);
         visited[src] = true;
         parent[src] = -1;
-        while(!q.empty()){
+        while (!q.empty()) {
             int frontNode = q.front();
             q.pop();
-            for(auto nbr : adjList[frontNode]){
-                if(!visited[nbr.first]){
+            for (auto nbr : adjList[frontNode]) {
+                if (!visited[nbr.first]) {
                     q.push(nbr.first);
                     visited[nbr.first] = true;
                     parent[nbr.first] = frontNode;
                 }
             }
         }
-        // Parent array is ready now
-        vector<int>ans;
+        // store path in ans, after traversing in the parent array
+        vector<int> ans;
         int node = dest;
-        while(node != -1){
+        while (node != -1) {
             ans.push_back(node);
             node = parent[node];
         }
         // reverse the ans
-        reverse(ans.begin(),ans.end());
-        for(auto i : ans){
-            cout<<i<<" ";
+        reverse(ans.begin(), ans.end());
+        for (auto i : ans) {
+            cout << i << " ";
         }
     }
 };
 
-int main(){
+int main() {
     Graph g;
-    g.addEdge(0,1,1,1);
-    g.addEdge(0,2,1,1);
-    g.addEdge(0,3,1,1);
-    g.addEdge(1,4,1,1);
-    g.addEdge(4,5,1,1);
-    g.addEdge(2,5,1,1);
-    g.addEdge(3,6,1,1);
-    g.addEdge(6,5,1,1);
+    g.addEdge(0, 1, 1, 1);
+    g.addEdge(0, 2, 1, 1);
+    g.addEdge(0, 3, 1, 1);
+    g.addEdge(1, 4, 1, 1);
+    g.addEdge(4, 5, 1, 1);
+    g.addEdge(2, 5, 1, 1);
+    g.addEdge(3, 6, 1, 1);
+    g.addEdge(6, 5, 1, 1);
+
     g.printAdjList();
+    
     int src = 0;
     int dest = 5;
-    cout<<"Printing shortest path -> ";
-    g.shortestPath(src,dest);
-    cout<<endl;
+
+    cout << "Printing shortest path -> ";
+    g.shortestPath(src, dest);
+    cout << endl;
     return 0;
 }
