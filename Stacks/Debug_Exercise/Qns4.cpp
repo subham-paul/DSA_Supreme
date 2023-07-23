f52a5fc7ce6af3c215692e9bce750164135e355e
subham-paul <subhampaul456@gmail.com>
2023-07-23 11:50:03 +0530

    WIP

--- Stacks/Debug_Exercise/Qns4.cpp
+++ Stacks/Debug_Exercise/Qns4.cpp
@@ -3,9 +3,8 @@ using namespace std;
 template <typename T>
 class Stack {
     Node<T> *head;
-    int size;
-
-    public :    
+    int size;		// number of elements prsent in stack
+    public :
     Stack() {
         head = NULL;
         size = 0;
@@ -28,17 +27,33 @@ class Stack {
     }
     
     T pop() {
+        if(size==0){
+            return 0;
+        }
         T data = head->data;
         head = head->next;
         size--;
-        return data;   
+        return data;
+        
     }
     
     T top() {
+        // Return 0 if stack is empty. Don't display any other message
+        if(size==0){
+            return 0;
+        }
         return head->data;
-    }    
+    }
 };
 int main(){
-
+    Stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    for (int i = 0; i < 5; i++) {
+        cout << s.top() << " ";
+        s.pop();
+    }
     return 0;
 }
