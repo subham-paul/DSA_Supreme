01824832adb5ba13de673c3c3daabcf78bb0d519
Subham <subhampaul456@gmail.com>
2023-08-20 14:34:27 +0530

    code update

--- 2D-Arrays/print_col_wise.cpp
+++ 2D-Arrays/print_col_wise.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 using namespace std;
-void printRowArray(int arr[][3], int row, int col)
-{
-
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
+void printColArray(int arr[][3], int row, int col) {
+    cout << "The 2D array outputs are below" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
     }
 }
-int main()
-{
+int main() {
     int arr[3][3];
     int row = 3;
     int col = 3;
-    cout << "Enter 3,3 2D array input : " << endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
+    cout << "Enter 3 Rows & 3 Cols 2D array input : " << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             cin >> arr[j][i];
         }
     }
-    printRowArray(arr, row, col);
+    printColArray(arr, row, col);
 }
