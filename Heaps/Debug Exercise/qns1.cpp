b6d627d2fe0edeab1b3c5ecb82820b75ba1ab687
subham-paul <subhampaul456@gmail.com>
2023-11-28 20:37:49 +0530

    wip

---
+++ Heaps/Debug Exercise/qns1.cpp
@@ -0,0 +1,22 @@
+void heapify(int arr[], int N, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < N && arr[l] > arr[largest])
+        largest = l;
+    if (r < N && arr[r] > arr[largest])
+        largest = r;
+    if (largest == i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, N, largest);
+    }
+}
+
+void heapSort(int arr[], int N) {
+    for (int i = N / 2 - 1; i >= 0; i--)
+        heapify(arr, N, i);
+    for (int i = 0; i < N - 1; i++) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
