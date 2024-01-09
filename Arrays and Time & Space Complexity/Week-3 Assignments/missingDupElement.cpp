71982f9c979c65d9d65534b64112e43efdde61e7
Subham <subhampaul456@gmail.com>
2024-01-09 22:14:33 +0530

    Extra spaces remove

--- Arrays and Time & Space Complexity/Week-3 Assignments/missingDupElement.cpp
+++ Arrays and Time & Space Complexity/Week-3 Assignments/missingDupElement.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 using namespace std;
-// Here we need to missing element of the array like 
+// Here we need to missing element of the array like
 // input arr[] = {1, 2, 2, 5, 4}; Output is 3 because 3 missing of this array
-void findMissingDuplicate(int arr[], int size)
-{
+void findMissingDuplicate(int arr[], int size) {
     int i = 0;
-    while (i < size)
-    {
+    while (i < size) {
         int index = arr[i] - 1;
-        if (arr[i] != arr[index])
-        {
+        if (arr[i] != arr[index]) {
             swap(arr[i], arr[index]);
-        }
-        else
-        {
+        } else {
             ++i;
         }
     }
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] != i + 1)
-        {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] != i + 1) {
             cout << i + 1 << " ";
         }
     }
     cout << endl;
 }
-int main()
-{
+
+int main() {
     int arr[] = {1, 2, 2, 5, 4};
     int size = sizeof(arr) / sizeof(int);
     findMissingDuplicate(arr, size);
