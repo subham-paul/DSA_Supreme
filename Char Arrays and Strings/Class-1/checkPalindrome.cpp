55a735c8a4844b9bcdd454e46c81c689713a014d
Subham <subhampaul456@gmail.com>
2024-02-06 14:41:35 +0530

    code modified

--- Char Arrays and Strings/Class-1/checkPalindrome.cpp
+++ Char Arrays and Strings/Class-1/checkPalindrome.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-bool checkPalindrome(char word[])
-{
+bool checkPalindrome(char word[]) {
     int i = 0;
     int n = strlen(word);
     int j = n - 1;
 
-    while (i <= j)
-    {
-        if (word[i] != word[j])
-        {
+    while (i <= j) {
+        if (word[i] != word[j]) {
             return false;
-        }
-        else
-        {
+        } else {
             i++;
             j--;
         }
     }
     return true;
 }
-int main()
-{
+int main() {
     char arr[100] = "leveljj";
-    cout << "plindrome check: " << checkPalindrome(arr)<<endl;
+    cout << "plindrome check: " << checkPalindrome(arr) << endl;
     return 0;
 }
