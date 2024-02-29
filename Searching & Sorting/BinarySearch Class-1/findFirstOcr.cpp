d869b62dc6b45e57117061869bdf4245369c2bf3
Subham <subhampaul456@gmail.com>
2024-02-29 22:31:47 +0530

    Empty space remove

--- Searching & Sorting/BinarySearch Class-1/findFirstOcr.cpp
+++ Searching & Sorting/BinarySearch Class-1/findFirstOcr.cpp
@@ -1,42 +1,36 @@
 #include <iostream>
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int firstOcc(vector<int> arr, int target) {
   int s = 0;
   int e = arr.size() - 1;
-  int mid = s + (e-s)/2;
+  int mid = s + (e - s) / 2;
   int ans = -1;
 
-  while(s <= e) {
-    if(arr[mid] == target) {
-      //ans store
-      ans = mid;
-      //left search
-      e = mid - 1;
+  while (s <= e) {
+    if (arr[mid] == target) {
+      ans = mid; // ans store      
+      e = mid - 1; // left search
+    } else if (target < arr[mid]) {      
+      e = mid - 1; // left me search
+    } else if (target > arr[mid]) {      
+      s = mid + 1; // right search
     }
-    else if(target < arr[mid] ) {
-      //left me search
-      e = mid - 1;
-    }
-    else if(target > arr[mid] ) {
-      //right search
-      s = mid + 1;
-    }
-    mid = s + (e-s)/2;
+    mid = s + (e - s) / 2;
   }
   return ans;
 }
 
 int main() {
-  vector<int> v{1,3,3,3,3,3,3,4,4,4,4,6,7};
+  vector<int> v{1, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 6, 7};
   int target = 4;
 
   int ans = firstOcc(v, target);
-  cout << "ans is. "<< ans << endl;
+  cout << "Find ans help of Manual Funtion => " << ans << endl;
 
-  auto ans2 = lower_bound (v.begin(), v.end(), target);
-  cout << "ans2 is " << ans2-v.begin() << endl;
+  auto ans2 = lower_bound(v.begin(), v.end(), target);
+  cout << "Find ans help of LOWER Bound Function => " << ans2 - v.begin() << endl;
   return 0;
 }
