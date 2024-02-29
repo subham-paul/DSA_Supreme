cee1a00fbba77826f1c0911b63d02af7c7555dac
Subham <subhampaul456@gmail.com>
2024-02-29 22:38:10 +0530

    remove empty space

--- Searching & Sorting/BinarySearch Class-1/binarySearch.cpp
+++ Searching & Sorting/BinarySearch Class-1/binarySearch.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include<algorithm>
-#include<vector>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int binarySearch(int arr[], int size, int target) {
   int start = 0;
   int end = size - 1;
-  int mid = start + (end - start ) / 2;
+  int mid = start + (end - start) / 2;
 
-  while(start <= end) {
+  while (start <= end) {
     int element = arr[mid];
-    if(element == target) {//element found, then return index
+    if (element == target) { // element found, then return index
       return mid;
     }
-    if(target < element) {
-      //search in left
-      end = mid - 1;
+    if (target < element) {
+      end = mid - 1; // search in left side
     } else {
-      //search in right
-      start = mid + 1;
+      start = mid + 1; // search in right side
     }
-
-    mid = start + (end - start ) / 2;
+    mid = start + (end - start) / 2;
   }
-
-  //element not found
-  return -1;
-
+  return -1; // element not found
 }
 
 int main() {
-  // int arr[] = {2,4,6,8,10,12,16};
+  // int arr[] = {2, 4, 6, 8, 10, 12, 16};
   // int size = 7;
   // int target = 20;
 
@@ -38,16 +32,15 @@ int main() {
 
   // if(indexOftarget == -1) {
   //   cout << "target not found" << endl;
-  // }
-  // else  {
+  // } else  {
   //   cout << "target found at " << indexOftarget <<" index " << endl;
   // }
 
-  vector<int> v{1,2,3,4,5,6};
-  int arr[] = {1,2,3,4,5,6,7 };
+  vector<int> v{1, 2, 3, 4, 5, 6};
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
   int size = 7;
 
-  if(binary_search(arr, arr + size, 7)) {
+  if (binary_search(arr, arr + size, 7)) {
     cout << "Found" << endl;
   } else {
     cout << "Not found. " << endl;
