cad3918e5da63f54b7d4b1d3629281583bfcc588
Subham <subhampaul456@gmail.com>
2024-03-01 17:49:41 +0530

    empty space remove

--- Searching & Sorting/BinarySearch Class-2/findPivot.cpp
+++ Searching & Sorting/BinarySearch Class-2/findPivot.cpp
@@ -2,36 +2,29 @@
 #include <vector>
 using namespace std;
 
-int findPivot(vector<int> arr)
-{
+int findPivot(vector<int> arr) {
     int s = 0;
     int e = arr.size() - 1;
     int mid = s + (e - s) / 2;
-    while (s <= e)
-    {
-        if (mid + 1 < arr.size() && arr[mid] > arr[mid + 1])
-        {
+    while (s <= e) {
+        if (mid + 1 < arr.size() && arr[mid] > arr[mid + 1]) {
             return mid;
         }
-        if (mid - 1 >= 0 && arr[mid - 1] > arr[mid])
-        {
+        if (mid - 1 >= 0 && arr[mid - 1] > arr[mid]) {
             return mid - 1;
         }
-        if (arr[s] >= arr[mid])
-        {
+        if (arr[s] >= arr[mid]) {
             e = mid - 1;
-        }
-        else
-        {
+        } else {
             s = mid + 1;
         }
         mid = s + (e - s) / 2;
     }
     return -1;
 }
-int main()
-{
-    vector<int> arr{1, 2, 3, 4, 5, 6};
+int main() {
+    vector<int> arr{3, 4, 5, 6, 7, 1, 2};
+    //              0, 1, 2, 3, 4, 5, 6
     int ans = findPivot(arr);
     cout << "Index of Pivot Element is " << ans << endl;
     return 0;
