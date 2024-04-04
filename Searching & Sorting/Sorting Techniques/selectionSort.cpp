f52575972a15339a3308c1fa70d2329a63d89b66
Subham <subhampaul456@gmail.com>
2024-04-04 09:23:56 +0530

    code optimise

--- Searching & Sorting/Sorting Techniques/selectionSort.cpp
+++ Searching & Sorting/Sorting Techniques/selectionSort.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(){
+int main() {
     vector<int> arr{5, 4, 3, 2, 1, -5, -2}; // worst case Time Complexity- O(n^2),  Space Complexity- O(1)
     int n = arr.size();
     // selection sort
     // outer loop -> (n-1) rounds
-    for (int i = 0; i < n - 1; i++)
-    {
+    for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         // inner Loop -> index of minimum element in range i->n
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[minIndex])
-            {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
                 // new minimum, then store
                 minIndex = j;
             }
@@ -27,11 +24,9 @@ int main(){
     // {
     //     cout << arr[i] << " ";
     // }
-    for (auto val : arr)
-    {
+    for (auto val : arr) {
         cout << val << ", ";
     }
     cout << endl;
-
     return 0;
 }
