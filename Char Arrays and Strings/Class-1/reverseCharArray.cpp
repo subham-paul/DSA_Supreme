1f5ef059e1d801fe281a3d9a5ee95ea5b518d873
Subham <subhampaul456@gmail.com>
2024-05-01 00:09:49 +0530

    code change

--- Char Arrays and Strings/Class-1/reverseCharArray.cpp
+++ Char Arrays and Strings/Class-1/reverseCharArray.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-void reverseCharArray(char name[])
-{
-        int left = 0;
-        int right = strlen(name)-1;
 
-        while (left <= right)
-        {
-            swap(name[left], name[right]);
-            left++;
-            right--;
-        }
+void reverseCharArray(char name[]) {
+    int left = 0;
+    int right = strlen(name) - 1;
+
+    while (left <= right) {
+        swap(name[left], name[right]);
+        left++;
+        right--;
+    }
 }
-int main()
-{
+
+int main() {
     char name[100];
-    cout<<"Enter name ";
+    cout << "Enter name ";
     cin >> name;
     reverseCharArray(name);
     cout << "After reversal process: " << name << endl;
