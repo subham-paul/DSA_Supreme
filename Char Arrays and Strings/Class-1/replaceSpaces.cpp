7095d6afdbca107e7bb6c45e960d9ebd409236b8
Subham <subhampaul456@gmail.com>
2024-05-10 11:15:16 +0530

    code modified

--- Char Arrays and Strings/Class-1/replaceSpaces.cpp
+++ Char Arrays and Strings/Class-1/replaceSpaces.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-void replaceSpaces(char sentence[])
-{
 
+void replaceSpaces(char sentence[]) {
     int i = 0;
     int n = strlen(sentence);
 
-    for (int i = 0; i < n; i++)
-    {
-        if (sentence[i] == ' ')
-        {
+    for (int i = 0; i < n; i++) {
+        if (sentence[i] == ' ') {
             sentence[i] = '@';
         }
     }
 }
-int main()
-{
+
+int main() {
     char sentence[100];
     cout << "Enter a Sentence ";
     cin.getline(sentence, 100);
     replaceSpaces(sentence);
-    cout << "printing sentence with @ char -> " << sentence << endl;
+    cout << "Printing sentence with @ char -> " << sentence << endl;
     return 0;
 }
