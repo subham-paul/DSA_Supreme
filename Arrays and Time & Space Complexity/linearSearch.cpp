eb0490927cd80890d5d430f571278a166e6fbe8f
Subham <subhampaul456@gmail.com>
2024-05-27 22:18:20 +0530

    code modified

--- Arrays and Time & Space Complexity/linearSearch.cpp
+++ Arrays and Time & Space Complexity/linearSearch.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
 using namespace std;
-bool linearSearch(int arr[], int size, int key)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == key)
-        {
+
+bool linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
             return true;
         }
     }
     return false;
 };
-int main()
-{
+
+int main() {
     int arr[] = {11, 22, 33, 44, 55, 66};
     int size = 6;
-    int key = 5;
-    if (linearSearch(arr, size, key))
-    {
-        cout << "Found value"<<endl;
-    }
-    else
-    {
-        cout << "Not Found Value"<<endl;
+    int key = 55;
+    if (linearSearch(arr, size, key)) {
+        cout << "Found value" << endl;
+    } else {
+        cout << "Not Found Value" << endl;
     }
     return 0;
 }
