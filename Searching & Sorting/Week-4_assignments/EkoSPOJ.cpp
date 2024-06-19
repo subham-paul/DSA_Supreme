8aa4227f3a0ce587a8bf1244017995c31abe6cc7
Subham <subhampaul456@gmail.com>
2024-06-19 22:01:50 +0530

    code optimise

--- Searching & Sorting/Week-4_assignments/EkoSPOJ.cpp
+++ Searching & Sorting/Week-4_assignments/EkoSPOJ.cpp
@@ -5,10 +5,8 @@ using namespace std;
 
 bool isPossibleSolution(vector<long long int> trees, long long int m, long long int mid) {
     long long int woodCollected = 0;
-    for (long long int i = 0; i < trees.size(); i++)
-    {
-        if (trees[i] > mid)
-        {
+    for (long long int i = 0; i < trees.size(); i++) {
+        if (trees[i] > mid) {
             woodCollected += trees[i] - mid;
         }
     }
@@ -18,16 +16,12 @@ bool isPossibleSolution(vector<long long int> trees, long long int m, long long 
 long long int maxSawBladeHeight(vector<long long int> trees, long long int m) {
     long long int start = 0, end, ans = -1;
     end = *max_element(trees.begin(), trees.end());
-    while (start <= end)
-    {
+    while (start <= end) {
         long long int mid = start + (end - start) / 2;
-        if (isPossibleSolution(trees, m, mid))
-        {
+        if (isPossibleSolution(trees, m, mid)) {
             ans = mid;
             start = mid + 1;
-        }
-        else
-        {
+        } else {
             end = mid - 1;
         }
     }
@@ -38,8 +32,7 @@ int main() {
     long long int n, m;
     cin >> n >> m;
     vector<long long int> trees;
-    while (n--)
-    {
+    while (n--) {
         long long int height;
         cin >> height;
         trees.push_back(height);
