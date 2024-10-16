3b8eadd700a8090fe1366de8b2d91e20194880cd
Subham <subhampaul456@gmail.com>
2024-10-16 13:55:22 +0530

    change code

--- Recursion/Class-1/printCounting.cpp
+++ Recursion/Class-1/printCounting.cpp
@@ -3,26 +3,21 @@ using namespace std;
 
 void printCounting(int n) {
 	//base case
-	if(n == 0) {
-		return;
-	}
+	if(n == 0) return;
 
 	//processing
 	cout << n << " ";
 	
-
 	//recursive relation
 	printCounting(n-1);	
 }
 
 int main() {
-
 	int n;
 	cout << "Enter the value of n  " << endl;
 	cin >> n;
 
 	printCounting(n);
 	cout << endl;
-
 	return 0;
 }
